f08bc78851ce3bf855a4aa3bfd0026e7bb50d079
mouadh7belgoumri <m_belgoumri@estin.dz>
2025-06-17 18:18:52 +0100

    frontend's repo initialization

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+// import { useState } from 'react'
+// import React from 'react'
+
+
+import './App.css'
+
+const App = () => {
+  return (
+    <div className='h-dvh '>
+      
+    </div>
+  )
+}
+
+
+export default App
