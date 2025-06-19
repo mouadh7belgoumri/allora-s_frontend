90dbb26262dc7a34fb1c30e34fcd72e8b6adaee2
mouadh7belgoumri <m_belgoumri@estin.dz>
2025-06-19 18:47:59 +0100

    starting hero section

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,13 +1,15 @@
 import HeroImage from '../assets/generated-image-1474ef88-294f-4c32-a4f2-8a1b36be1cd2.png'
+import { Link } from 'react-router'
 
 export default function Hero() {
   return (
-    <div>
-        <img src={HeroImage} alt="HeroImage" className='w-full h-260' />
-        <div className="flex flex-row w-1/2">
-            <h5>Joyful Experience</h5>
-            <h1>Allora exists for the sake of making children happy. “joy” is what we aim at</h1>
-        </div>
+    <div className='relative '>
+      <img src={HeroImage} alt="HeroImage" className='w-full'/>
+      <div className='absolute top-[50%] -mt-40 w-10/33 flex flex-col gap-10 ml-30 '>
+        <h4 className='text-2xl font-bold text-shadow-[#FF7878] text-shadow-lg/20   '>Joyful experience</h4>
+        <h1 className='text-[#FF6265] font-extrabold text-[40px] text-shadow-black text-shadow-lg/50 '>Allora Exists For The Sake Of Making Children Happy. “Joy” Is What We Aim At</h1>
+        {/* <Link to='/Shop'>jgu</Link> */}
+      </div>
     </div>
   )
 }
